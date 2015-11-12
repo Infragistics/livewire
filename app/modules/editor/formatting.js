5e9e8eff486ec0505e7295518d8fddba33c7f976
Vasya Kacheshmarova <vasq1989@gmail.com>
2015-11-12 19:43:18 +0200

    adding link dialog with file picker

--- app/modules/editor/formatting.js
+++ app/modules/editor/formatting.js
@@ -8,14 +8,17 @@ var
 	messenger = require(path.resolve(__dirname, '../messenger')),
 	_ = require('lodash'),
 	config = require(path.resolve(__dirname, '../config')).get(),
-	formatter = require(path.resolve(__dirname, '../formats')).get(config.defaultFormat);
+	formatter = require(path.resolve(__dirname, '../formats')).get(config.defaultFormat),
+	remote = require('remote'),
+	nodeDialog = remote.require('dialog');
 
 messenger.subscribe.file('formatChanged', function(data, envelope){
 	formatter = data;
 });
 
 messenger.subscribe.format('wrapText', function(data, envelope){  
-	wrapSelectedText(formatter.shortcuts[envelope.data.shortcut]);
+	editor.commands.exec(envelope.data.shortcut);
+	//wrapSelectedText(formatter.shortcuts[envelope.data.shortcut]);
 	editor.focus();
 });
 
@@ -34,6 +37,7 @@ var wrapSelectedText = function(format){
 			editor.selection.selectLine();
 			range = editor.getSelectionRange();
 		}
+		
 	}
 	
 	selectedText = editor.session.getTextRange(range);
@@ -68,6 +72,61 @@ var buildCommand = function(name, shortcut){
 	}
 };
 
+var buildDialogCommand = function(name, shortcut){
+	return {
+		name: name,
+		bindKey: { 
+			win: shortcut.replace(/Command/, 'Ctrl'), 
+			mac: shortcut.replace(/Ctrl/, 'Command') },
+		exec: function(){
+			var callback = function (filePaths) {
+				var filePath, format;
+				if (!filePaths || !filePaths.length) {
+					console.log('No file paths selected');
+				} else {
+					if (typeof filePaths === "string") {
+						filePath = filePaths;
+					} else {
+						filePath = path.basename(filePaths[0]);
+					}
+					
+					// transform file name:
+					filePath = filePath.replace(".md", ".html").replace(".adoc", ".html");
+					
+					format = $.extend({}, formatter.shortcuts[name]);
+					format.right = format.right.replace("{0}", filePath);
+					wrapSelectedText(format);
+				}
+				$("#browseDialog").modal('hide');
+				$("#linkInput").val("").off("keyup");
+				editor.focus(); 
+			};
+			
+			$("#browseDialog").modal().on('shown.bs.modal', function (e) {
+				$("#linkInput").focus().on("keyup", function(e){
+					if (e.keyCode === 13) {
+						callback($("#linkInput").val());
+					}
+				});
+			});
+			
+			$("#browseBtn").one("click", function(){
+				var options = {
+					title: 'Open Markdown Files',
+					properties: ['openFile'],
+					//TODO: use formatter settings for name and extensions
+					filters: [{ name: 'Markdown', extensions: ['md'] }, { name: 'AsciiDoc', extensions: ['adoc'] }]
+				};
+				
+				nodeDialog.showOpenDialog(options, callback);
+			});
+			$("#doneBtn").one("click", function(){
+				callback($("#linkInput").val());
+			});
+		}
+	}
+};
+
 module.init = function (editorInstance) {
 	
 	editor = editorInstance;
@@ -75,7 +134,7 @@ module.init = function (editorInstance) {
 	editor.commands.addCommand(buildCommand('bold', 'Ctrl-B'));
 	editor.commands.addCommand(buildCommand('italic', 'Ctrl-I'));
 	editor.commands.addCommand(buildCommand('code', 'Ctrl-D'));
-	editor.commands.addCommand(buildCommand('link', 'Ctrl-K'));
+	editor.commands.addCommand(buildDialogCommand('link', 'Ctrl-K'));
 	editor.commands.addCommand(buildCommand('image', 'Ctrl-Shift-I'));
 	editor.commands.addCommand(buildCommand('h1', 'Ctrl-1'));
 	editor.commands.addCommand(buildCommand('h2', 'Ctrl-2'));
