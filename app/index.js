b4780294e29d4e20ee6e650c4fe862080bf20a0d
Craig Shoemaker <craig@craigshoemaker.net>
2015-12-28 15:11:05 -0800

    add split-controller

--- app/index.js
+++ app/index.js
@@ -1,3 +1,29 @@
+/* global appSettings */
+
+window.appSettings = {
+    _editorWidth: '49%',
+    
+    editorWidth: function(value){
+        if(value){
+            appSettings._editorWidth = value;
+        } else {
+            return appSettings._editorWidth;
+        }
+    },
+    
+    split: function(){
+        return (parseInt(appSettings._editorWidth) + 1) + '%';
+    },
+    
+    resultsWidth: function(value){
+        if(parseInt(appSettings.editorWidth()) > 90){
+            return '0px';
+        } else {
+            return (100 - parseInt(appSettings.split)) + '%';
+        }
+    }
+};
+
 require.main.require('./vendor/ace-spell-check/spellcheck_ace.js');
 require.main.require('./modules/menu');
 require.main.require('./modules/toolbars/formatting');
