b10ae7cd7839e4667dc657b38ae83c50cff6d506
Craig Shoemaker <craig@craigshoemaker.net>
2016-02-02 11:03:53 -0800

    add loop to include logic to make updates easier

--- app/modules/formats/grammar/asciidoc/include/index.js
+++ app/modules/formats/grammar/asciidoc/include/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const INCLUDE_SUPPORT_DEPTH = 4;
+
 var includeFile = (asciidoc, sourceFolder) => {
     var filePath;
     
@@ -17,12 +19,9 @@ var includeFile = (asciidoc, sourceFolder) => {
 
 module.exports.apply = (asciidoc, sourceFolder) => {
     
-    // keeping it simple ;)
-    // includes are only supported 4 levels deep
-    asciidoc = includeFile(asciidoc, sourceFolder);
-    asciidoc = includeFile(asciidoc, sourceFolder);
-    asciidoc = includeFile(asciidoc, sourceFolder);
-    asciidoc = includeFile(asciidoc, sourceFolder);
+    for (var i = 0; i < INCLUDE_SUPPORT_DEPTH; i++) {
+        asciidoc = includeFile(asciidoc, sourceFolder);
+    }
     
     return asciidoc;    
 }; 
