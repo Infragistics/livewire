6038bf3d7ebf9187fc586489d2fe2258a913a4b0
Craig Shoemaker <craig@craigshoemaker.net>
2016-02-05 10:36:15 -0800

    created modular system for formatting dialogs

---
+++ app/modules/editor/dialogs/links/index.js
@@ -0,0 +1,86 @@
+module = module.exports;
+
+const 
+    path = require('path'),
+    messenger = require(path.resolve(__dirname, '../../../messenger')),
+    remote = require('remote'),
+    nodeDialog = remote.require('dialog'),
+    fileNameCleaner =  require('../../fileNameCleaner.js');
+    
+var 
+    $browseDialog, 
+    $linkInput, 
+    $browseBtn, 
+    $doneBtn,   
+    formatterModule;
+
+var buildDialogCommand = function(name, shortcut){
+	return {
+		name: name,
+		bindKey: { 
+			win: shortcut.replace(/Command/, 'Ctrl'), 
+			mac: shortcut.replace(/Ctrl/, 'Command') },
+		exec: function(){
+			$browseDialog.modal();
+		}
+	}
+};
+
+var createLink = (filePaths) => {
+    var filePath, format, side;
+    if (!filePaths || !filePaths.length) {
+        console.log('No file paths selected');
+    } else {
+        if (typeof filePaths === 'string') {
+            filePath = filePaths;
+        } else {
+            filePath = path.basename(filePaths[0]);
+        }
+        
+        // transform file name:
+        filePath = filePath.replace(new RegExp('\\.(' + formatterModule.formatter.extensions.join('|') + ')$'), '.html');
+        filePath = fileNameCleaner.clean(filePath);
+
+        format = $.extend({}, formatterModule.formatter.shortcuts.link);
+
+        side = /\{0\}/.test(format.left) ? 'left' : 'right';
+
+        format[side] = format[side].replace('{0}', filePath);
+        formatterModule.wrapSelectedText(format);
+    }
+    $browseDialog.modal('hide');
+    $linkInput.val('').off('keyup');
+    formatterModule.editor.focus();
+};
+
+module.init = (formatterMod) => {
+    formatterModule = formatterMod;
+    formatterModule.editor.commands.addCommand(buildDialogCommand('link', 'Ctrl-K'));
+    
+    $browseDialog = $('#browseDialog');
+    $linkInput = $('#linkInput');
+    $browseBtn = $('#browseBtn');
+    $doneBtn = $('#doneBtn');
+
+    $browseDialog.on('shown.bs.modal', (e) => {
+        $linkInput.focus();
+        $linkInput.on('keyup', (e) => {
+            if (e.keyCode === 13) {
+                createLink($linkInput.val());
+            }
+        });
+    });
+
+    $browseBtn.one('click', () => {
+        var options = {
+            title: 'Select A File',
+            properties: ['openFile'],
+            filters: [{ name: formatterModule.formatter.name, extensions: formatterModule.formatter.extensions }]
+        };
+        nodeDialog.showOpenDialog(options, createLink);
+    });
+    
+    $doneBtn.one('click', () => {
+        createLink($linkInput.val());
+    });
+};
--- app/modules/editor/formatting.js
+++ app/modules/editor/formatting.js
@@ -2,174 +2,128 @@
 	
 module = module.exports;
 
-
 var 
-	editor = null,
 	path = require('path'),
 	messenger = require(path.resolve(__dirname, '../messenger')),
 	_ = require('lodash'),
-	config = require(path.resolve(__dirname, '../config')).get(),
-	formatter = require(path.resolve(__dirname, '../formats')).get(config.defaultFormat),
-	remote = require('remote'),
-	nodeDialog = remote.require('dialog'),
-	fileNameCleaner =  require('./fileNameCleaner.js');
-	
-var $browseDialog, $linkInput, $browseBtn, $doneBtn;
+    fs = require('fs'),
+	config = require(path.resolve(__dirname, '../config')).get();
+    
+var dialogs = {
+    links: require(path.resolve(__dirname, './dialogs/links/index.js'))
+};
 
-$(function(){
-	$browseDialog = $("#browseDialog");
-	$linkInput = $("#linkInput");
-	$browseBtn = $("#browseBtn");
-	$doneBtn = $("#doneBtn");
-	
-	var createLink = function (filePaths) {
-		var filePath, format, side;
-		if (!filePaths || !filePaths.length) {
-			console.log('No file paths selected');
-		} else {
-			if (typeof filePaths === 'string') {
-				filePath = filePaths;
-			} else {
-				filePath = path.basename(filePaths[0]);
-			}
-			
-			// transform file name:
-			filePath = filePath.replace(new RegExp('\\.(' + formatter.extensions.join('|') + ')$'), '.html');
-			filePath = fileNameCleaner.clean(filePath);
-			
-			format = $.extend({}, formatter.shortcuts.link);
-			
-			side = /\{0\}/.test(format.left) ? 'left': 'right'; 
-			
-			format[side] = format[side].replace('{0}', filePath);
-			wrapSelectedText(format);
-		}
-		$browseDialog.modal('hide');
-		$linkInput.val('').off('keyup');
-		editor.focus(); 
-	};
-	
-	$browseDialog.on('shown.bs.modal', function (e) {
-		$linkInput.focus();
-		$linkInput.on('keyup', function(e){
-			if (e.keyCode === 13) {
-				createLink($linkInput.val());
-			}
-		});
-	});
-	
-	$browseBtn.one('click', function(){
-		var options = {
-			title: 'Select A File',
-			properties: ['openFile'],
-			filters: [{ name: formatter.name, extensions: formatter.extensions }]
-		};
-		nodeDialog.showOpenDialog(options, createLink);
-	});
-	$doneBtn.one('click', function(){
-		createLink($linkInput.val());
-	});
+var $dialogsContainer;
+
+module.editor = null;
+module.formatter = require(path.resolve(__dirname, '../formats')).get(config.defaultFormat);
+  
+messenger.subscribe.file('formatChanged', (data, envelope) => {
+	module.formatter = data;
 });
 
-messenger.subscribe.file('formatChanged', function(data, envelope){
-	formatter = data;
-});
-
-messenger.subscribe.format('wrapText', function(data, envelope){  
-	editor.commands.exec(envelope.data.shortcut);
+messenger.subscribe.format('wrapText', (data, envelope) => {  
+	module.editor.commands.exec(envelope.data.shortcut);
 	
 	// todo: make this generic if more dialogs are
 	// added to the editor
 	if(envelope.data.shortcut !== 'link'){
-		editor.focus();
+		module.editor.focus();
 	}
 });
 
-var isRangeEmpty = function(range){
+var isRangeEmpty = (range) => {
 	return range.end.column === range.start.column;
 };
 
-var wrapSelectedText = function(format){
+module.wrapSelectedText = (format) => {
 	var range, selectedText, wrapAtBeginningOfLine = false;
 	
-	range = editor.getSelectionRange();	
+	range = module.editor.getSelectionRange();	
 	
 	if(isRangeEmpty(range) && format.cursorOffset){
 		if(format.cursorOffset.wrapAtBeginningOfLine){
 			wrapAtBeginningOfLine = true;
-			editor.selection.selectLine();
-			range = editor.getSelectionRange();
+			module.editor.selection.selectLine();
+			range = module.editor.getSelectionRange();
 		}
 	}
 	
-	selectedText = editor.session.getTextRange(range);
+	selectedText = module.editor.session.getTextRange(range);
 	
-	editor.getSession().replace(range, format.left + selectedText + format.right);
+	module.editor.getSession().replace(range, format.left + selectedText + format.right);
 	
 	if(!_.isUndefined(format.cursorOffset)){
 		if(format.cursorOffset.fromLeft){
-			editor.getSelection().moveCursorTo(range.start.row, range.start.column + format.cursorOffset.value);
+			module.editor.getSelection().moveCursorTo(range.start.row, range.start.column + format.cursorOffset.value);
 		} else {
-			range = editor.getSelectionRange();
-			editor.getSelection().moveCursorTo(range.start.row, range.end.column - format.cursorOffset.value);			
+			range = module.editor.getSelectionRange();
+			module.editor.getSelection().moveCursorTo(range.start.row, range.end.column - format.cursorOffset.value);			
 		}
-		editor.clearSelection();
+		module.editor.clearSelection();
 	}
 	
 	if(wrapAtBeginningOfLine){
-		editor.selection.moveCursorLineEnd();
+		module.editor.selection.moveCursorLineEnd();
 	}
 	
 };
 
-var buildCommand = function(name, shortcut){
+var buildCommand = (name, shortcut) => {
 	return {
 		name: name,
 		bindKey: { 
 			win: shortcut.replace(/Command/, 'Ctrl'), 
 			mac: shortcut.replace(/Ctrl/, 'Command') },
-		exec: function(){
-			wrapSelectedText(formatter.shortcuts[name]);
+		exec: () => {
+			module.wrapSelectedText(module.formatter.shortcuts[name]);
 		}
 	}
 };
 
-var buildDialogCommand = function(name, shortcut){
-	return {
-		name: name,
-		bindKey: { 
-			win: shortcut.replace(/Command/, 'Ctrl'), 
-			mac: shortcut.replace(/Ctrl/, 'Command') },
-		exec: function(){
-			$browseDialog.modal();
-		}
-	}
+var registerDialog = (name, filePath) => {
+    var html = $dialogsContainer.html(); 
+    
+    fs.readFile(path.resolve(__dirname, filePath), 'utf8', (error, templateHtml) => {
+        if(error) {
+            console.log(error);
+        } else {
+            $dialogsContainer.html(html + templateHtml);
+            dialogs[name].init(module);
+        };
+    });
 };
 
-module.init = function (editorInstance) {
+$(() => {
+    $dialogsContainer = $('#dialogs-container');
+    
+    registerDialog('links', './dialogs/links/template.html');
+});
+
+
+module.init = (editorInstance) => {
 	
-	editor = editorInstance;
+	module.editor = editorInstance;
+    	
+	module.editor.commands.addCommand(buildCommand('bold', 'Ctrl-B'));
+	module.editor.commands.addCommand(buildCommand('italic', 'Ctrl-I'));
+	module.editor.commands.addCommand(buildCommand('code', 'Ctrl-D'));
+	module.editor.commands.addCommand(buildCommand('image', 'Ctrl-Shift-I'));
+	module.editor.commands.addCommand(buildCommand('h1', 'Ctrl-1'));
+	module.editor.commands.addCommand(buildCommand('h2', 'Ctrl-2'));
+	module.editor.commands.addCommand(buildCommand('h3', 'Ctrl-3'));
+	module.editor.commands.addCommand(buildCommand('quote', 'Ctrl-\''));
+	module.editor.commands.addCommand(buildCommand('unordered', 'Ctrl-.'));
+	module.editor.commands.addCommand(buildCommand('ordered', 'Ctrl-,'));
+	module.editor.commands.addCommand(buildCommand('hr', 'Ctrl--'));
 	
-	editor.commands.addCommand(buildCommand('bold', 'Ctrl-B'));
-	editor.commands.addCommand(buildCommand('italic', 'Ctrl-I'));
-	editor.commands.addCommand(buildCommand('code', 'Ctrl-D'));
-	editor.commands.addCommand(buildDialogCommand('link', 'Ctrl-K'));
-	editor.commands.addCommand(buildCommand('image', 'Ctrl-Shift-I'));
-	editor.commands.addCommand(buildCommand('h1', 'Ctrl-1'));
-	editor.commands.addCommand(buildCommand('h2', 'Ctrl-2'));
-	editor.commands.addCommand(buildCommand('h3', 'Ctrl-3'));
-	editor.commands.addCommand(buildCommand('quote', 'Ctrl-\''));
-	editor.commands.addCommand(buildCommand('unordered', 'Ctrl-.'));
-	editor.commands.addCommand(buildCommand('ordered', 'Ctrl-,'));
-	editor.commands.addCommand(buildCommand('hr', 'Ctrl--'));
-	
-	editor.commands.addCommand({
+	module.editor.commands.addCommand({
 		name: 'help', // pass through for help dialog
 		bindKey: {
 			win: 'ctrl+shift+/',
 			mac: 'cmd+shift+?'
 		},
-		exec: function(){
+		exec: () => {
 			messenger.publish.dialog('help.open');
 		}
 	});
