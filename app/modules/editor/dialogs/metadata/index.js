2d6b9fe2453676ac379bdf490f3173f6a59a96fc
Craig Shoemaker <craig@craigshoemaker.net>
2016-02-23 10:36:46 -0800

    implement control names typeahead

--- app/modules/editor/dialogs/metadata/index.js
+++ app/modules/editor/dialogs/metadata/index.js
@@ -1,12 +1,16 @@
 module = module.exports;
 
 const
+    _ = require('lodash'),
     path = require('path'),
     data = require(path.resolve(__dirname, '../../../../data'));
 
 var 
     $dialog,
-    $doneButton;
+    $doneButton,
+    $controlNameBox,
+    
+    _controls;
 
 var openDialog = () => {
     $dialog.modal();
@@ -16,16 +20,50 @@ module.init = (formatterModule, dialogModule) => {
     
     $dialog = $('#metadata-dialog');
     $doneButton = $('#metadata-done-button');
+    $controlNameBox = $('#metadata-control-name-box');
     
     formatterModule.editor.commands.addCommand(
         dialogModule.buildDialogCommand('image', 'Ctrl-Shift-M', openDialog));
         
     data.getControls().then((controls => {
-        console.log(controls);
+        _controls = controls;
+        
+        var allControls = [];
+        
+        var keys = _.keys(_controls);
+        keys.forEach((key) => {
+            allControls = allControls.concat(_controls[key]);
+        });
+        
+        $controlNameBox.typeahead(
+            { hint: true, highlight: true, minLength: 1 },
+            { name: 'controls', source: substringMatcher(allControls) });
     }));
         
     $doneButton.click((e) => {
         
         $dialog.modal('hide');
     })
+};
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
 };
