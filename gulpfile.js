0e0d1bfabe575fcf05dfbfe73da421c2a2a19237
Craig Shoemaker <craig@craigshoemaker.net>
2017-06-22 11:33:39 -0700

    remove gulp install task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 const gulp = require('gulp'); 
 const less = require('gulp-less');
-const path = require('path');
-const shell = require('shelljs');
 
 gulp.task('less', function () {
   return gulp.src('./app/index.less')
     .pipe(less())
     .pipe(gulp.dest('./app'));
-});
-
-gulp.task('install', function(){
-  
-  shell.cd('app');
-  shell.exec('npm install');
-  shell.exec('bower install');
-  
-  console.log();
-  console.log('************************************************');
-  console.log('       Livewire installed');
-  console.log('************************************************');
-  console.log();
-  
-  return;
 });
